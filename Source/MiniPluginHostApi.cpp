333626236be2519ad8de51b32e3e9361027c58ea
Daniel Rothmann <daniel@danielrothmann.com>
2017-11-05 19:19:25 +0100

    Implemented API

---
+++ Source/MiniPluginHostApi.cpp
@@ -0,0 +1,140 @@
+/*
+	==============================================================================
+
+	MiniPluginHostAPI
+	by Daniel Rothmann
+
+	A native API exposing static plugin host methods for interop
+
+	==============================================================================
+*/
+
+#include "MiniPluginHostApi.h"
+
+extern "C" {
+	PluginsManager* CreatePluginsManager()
+	{
+		return new PluginsManager();
+	}
+
+	void DeletePluginsManager(PluginsManager* manager)
+	{
+		delete manager;
+	}
+
+	void ScanAndAdd(PluginsManager* manager, char* filePath, bool dontRescanIfAlreadyInList)
+	{
+		manager->scanAndAdd(filePath, dontRescanIfAlreadyInList);
+	}
+
+	void RescanAll(PluginsManager* manager)
+	{
+		manager->rescanAll();
+	}
+
+	void AddToBlacklist(PluginsManager* manager, char* pluginId)
+	{
+		manager->addToBlacklist(pluginId);
+	}
+
+	void RemoveFromBlacklist(PluginsManager* manager, char* pluginId)
+	{
+		manager->removeFromBlacklist(pluginId);
+	}
+
+	void ClearBlacklist(PluginsManager* manager)
+	{
+		manager->clearBlacklist();
+	}
+
+	void CreateListFromXml(PluginsManager* manager, char* xmlKnownPluginsList)
+	{
+		manager->createListFromXml(xmlKnownPluginsList);
+	}
+
+	const char* CreateXmlFromList(PluginsManager* manager)
+	{
+		return manager->createXmlFromList();
+	}
+
+	const char* GetXmlPluginDescriptionForFile(PluginsManager* manager, char* filePath)
+	{
+		return manager->getXmlPluginDescriptionForFile(filePath);
+	}
+
+	const char* GetXmlPluginDescriptionForId(PluginsManager* manager, char* pluginId)
+	{
+		return manager->getXmlPluginDescriptionForId(pluginId);
+	}
+
+	PluginHost* CreatePluginHost()
+	{
+		return new PluginHost();
+	}
+
+	void DeletePluginHost(PluginHost* host)
+	{
+		delete host;
+	}
+
+	bool InstantiatePlugin(PluginHost* host, char* xmlPluginDescription, double sampleRate, int bufferSize)
+	{
+		return host->instantiatePlugin(xmlPluginDescription, sampleRate, bufferSize);
+	}
+
+	bool SuspendPlugin(PluginHost* host, bool shouldBeSuspended)
+	{
+		return host->suspendPlugin(shouldBeSuspended);
+	}
+
+	bool ReleasePlugin(PluginHost* host)
+	{
+		return host->releasePlugin();
+	}
+
+	void ProcessBlock(PluginHost* host, float* buffer, int bufferSize, int numChannels)
+	{
+		host->processBlock(buffer, bufferSize, numChannels);
+	}
+
+	int GetNumInputs(PluginHost* host)
+	{
+		return host->getNumInputs();
+	}
+
+	int GetNumOutputs(PluginHost* host)
+	{
+		return host->getNumOutputs();
+	}
+
+	int GetNumParameters(PluginHost* host)
+	{
+		return host->getNumParameters();
+	}
+
+	const char* GetParameterName(PluginHost* host, int index)
+	{
+		return host->getParameterName(index);
+	}
+
+	float GetValueByName(PluginHost* host, char* name)
+	{
+		return host->getValueByName(name);
+	}
+
+	bool SetValueByName(PluginHost* host, char* name, float value)
+	{
+		return host->setValueByName(name, value);
+	}
+
+	float GetValueByIndex(PluginHost* host, int index)
+	{
+		return host->getValueByIndex(index);
+	}
+
+	bool SetValueByIndex(PluginHost* host, int index, float value)
+	{
+		return host->setValueByIndex(index, value);
+	}
+
+}
---
+++ Source/MiniPluginHostApi.h
@@ -0,0 +1,47 @@
+/*
+  ==============================================================================
+
+    MiniPluginHostAPI
+	by Daniel Rothmann
+
+	A native API exposing static plugin host methods for interop
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "PluginsManager.h"
+#include "PluginHost.h"
+
+#define HOST_API __declspec(dllexport)
+
+extern "C" {
+	// Exposed methods related to plugins manager
+	HOST_API PluginsManager* CreatePluginsManager();
+	HOST_API void DeletePluginsManager(PluginsManager* manager);
+	HOST_API void ScanAndAdd(PluginsManager* manager, char* filePath, bool dontRescanIfAlreadyInList);
+	HOST_API void RescanAll(PluginsManager* manager);
+	HOST_API void AddToBlacklist(PluginsManager* manager, char* pluginId);
+	HOST_API void RemoveFromBlacklist(PluginsManager* manager, char* pluginId);
+	HOST_API void ClearBlacklist(PluginsManager* manager);
+	HOST_API void CreateListFromXml(PluginsManager* manager, char* xmlKnownPluginsList);
+	HOST_API const char* CreateXmlFromList(PluginsManager* manager);
+	HOST_API const char* GetXmlPluginDescriptionForFile(PluginsManager* manager, char* filePath);
+	HOST_API const char* GetXmlPluginDescriptionForId(PluginsManager* manager, char* pluginId);
+
+	// Exposed methods related to plugin host
+	HOST_API PluginHost* CreatePluginHost();
+	HOST_API void DeletePluginHost(PluginHost* host);
+	HOST_API bool InstantiatePlugin(PluginHost* host, char* xmlPluginDescription, double sampleRate, int bufferSize);
+	HOST_API bool SuspendPlugin(PluginHost* host, bool shouldBeSuspended);
+	HOST_API bool ReleasePlugin(PluginHost* host);
+	HOST_API void ProcessBlock(PluginHost* host, float* buffer, int bufferSize, int numChannels);
+	HOST_API int GetNumInputs(PluginHost* host);
+	HOST_API int GetNumOutputs(PluginHost* host);
+	HOST_API int GetNumParameters(PluginHost* host);
+	HOST_API const char* GetParameterName(PluginHost* host, int index);
+	HOST_API float GetValueByName(PluginHost* host, char* name);
+	HOST_API bool SetValueByName(PluginHost* host, char* name, float value);
+	HOST_API float GetValueByIndex(PluginHost* host, int index);
+	HOST_API bool SetValueByIndex(PluginHost* host, int index, float value);
+}
